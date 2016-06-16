2231f9e2e3e51ae522b25ecffaebf55867ee4f1f
Emilio Kim <sushimellow@me.com>
2016-06-16 16:30:01 -0500

    saved files exercise 22

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,8 +1,8 @@
- var animatePoints = function() {
- 
+var animatePoints = function() {
      var points = document.getElementsByClassName('point');
  
      var revealFirstPoint = function() {
+         console.log(points[0]);
          points[0].style.opacity = 1;
          points[0].style.transform = "scaleX(1) translateY(0)";
          points[0].style.msTransform = "scaleX(1) translateY(0)";
