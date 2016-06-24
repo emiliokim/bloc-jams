b49c57b60809558dfdd9fd60b2e223c61c58f453
Emilio Kim <sushimellow@me.com>
2016-06-24 16:29:45 -0500

    fixed index on javacript

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,10 +2,10 @@ var animatePoints = function() {
      var points = document.getElementsByClassName('point');
  
      var revealPoint = function(index) {
-         points[0].style.opacity = 1;
-         points[0].style.transform = "scaleX(1) translateY(0)";
-         points[0].style.msTransform = "scaleX(1) translateY(0)";
-         points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
+         points[index].style.opacity = 1;
+         points[index].style.transform = "scaleX(1) translateY(0)";
+         points[index].style.msTransform = "scaleX(1) translateY(0)";
+         points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
      }
      
      for (var i=0; i< points.length; i++){
